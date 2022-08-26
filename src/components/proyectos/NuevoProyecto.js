79bd8b072d95ea7c203a27cfbe707ed1ce44b6a8
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-26 14:55:29 -0300

    Se agregó el state para NuevoProyecto

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,17 +1,48 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NuevoProyecto = () => {
+  //State para proyecto
+  //Es un objeto y no un string porque debemos darle un id para que no halla problema por dos proyectos con el mismo nombre. El id se dará con una librería
+  const [proyecto, guardarProyecto] = useState({
+    nombre: "",
+    // id: "",
+  });
+
+  //Extraer nombre de proyecto
+  const { nombre } = proyecto;
+
+  //Leer el contenido del input
+  const onChangeProyecto = (e) => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Cuando el usuario envía un proyecto
+  const onSubmitProyecto = (e) => {
+    e.preventDefault();
+
+    //Validar el proyecto
+
+    //Agregar al state
+
+    //Reiniciar el form
+  };
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo proyecto
       </button>
-      <form className="formulario-nuevo-proyecto">
+      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
         <input
           type="text"
           className="input-text"
           placeholder="Nombre Proyecto"
           name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
         />
         <input
           type="submit"
