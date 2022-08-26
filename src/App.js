8269afc09b0351b55d43e9eed2d9b66cbf2fa2ad
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-26 13:14:01 -0300

    Se agregó el router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Login from "./components/auth/Login";
+import NuevaCuenta from "./components/auth/NuevaCuenta";
+import Proyectos from "./components/proyectos/Proyectos";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <h1>MERN Tasks</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/nueva-cuenta" element={<NuevaCuenta />} />
+        <Route path="/proyectos" element={<Proyectos />} />
+      </Routes>
+    </Router>
   );
 }
 
