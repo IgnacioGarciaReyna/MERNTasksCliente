11c1938db5108723ca92c2f7f18b7d1a713744f2
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-30 18:14:19 -0300

    Se agregó el html del componente Tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -21,6 +21,10 @@ const ListadoTareas = () => {
           tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
         )}
       </ul>
+
+      <button type="button" className="btn btn-eliminar">
+        Eliminar Proyecto &times;
+      </button>
     </Fragment>
   );
 };
