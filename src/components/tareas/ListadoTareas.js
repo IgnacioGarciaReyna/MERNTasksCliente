8bf998030393b5e0bfd806c86113034c7e150504
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-09-05 23:58:57 -0300

    Se agregó mostrar el nombre del proyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,19 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 import Tarea from "./Tareas";
 
 const ListadoTareas = () => {
+  //Extraer proyectos de state inicial
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Validación. Si no hay ningún proyecto seleccionado
+  if (!proyecto) return <h2>Selecciona un proyecto</h2>;
+
+  //Array destructuring -> para extraer el proyecto actual
+  //Extraemos la posición 0. Si hubiera mas elementos, se agregan mas separados con comas y se extraen esas otras posiciones
+  const [proyectoActual] = proyecto;
+
   const tareasProyecto = [
     { id: 1, nombre: "Elegir Plataforma", estado: true },
     { id: 2, nombre: "Elegir Colores", estado: false },
@@ -11,7 +23,7 @@ const ListadoTareas = () => {
 
   return (
     <Fragment>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {tareasProyecto.length === 0 ? (
           <li className="tarea">
