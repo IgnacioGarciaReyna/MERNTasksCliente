dec4715e2272d45534c7d1eba2324e509e184df1
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-09-06 00:03:28 -0300

    Se ocultó el formulario si no hay proyecto seleccionado

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const FormTarea = () => {
+  //Extraer si hay un proyecto activo
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Validación. Si no hay ningún proyecto seleccionado
+  if (!proyecto) return null;
+
+  //Array destructuring -> para extraer el proyecto actual
+  //Extraemos la posición 0. Si hubiera mas elementos, se agregan mas separados con comas y se extraen esas otras posiciones
+  const [proyectoActual] = proyecto;
+
   return (
     <div className="formulario">
       <form>
